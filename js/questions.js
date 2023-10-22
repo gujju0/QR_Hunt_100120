1ebd377290f03aac78613f5098ab1ec4a927e821
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:24:47 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Who developed Yahoo?",
+    answer: "System",
+    options: [
+      "AV103-B",
+      "AV009-A",
+      "AV008-D",
+      "AV109-C"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Made from a variety of materials, such as carbon, which inhibits the flow of current...?",
+    answer: "Version",
+    options: [
+      "AV010-D",
+      "Av108-A",
+      "AV014-C",
+      "AV110-A"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The most common format for a home video recorder is VHS. VHS stands for...?",
+    answer: "Research",
+    options: [
+      "AV001-C",
+      "AV101-C",
+      "AV008-A",
+      "AV012-B"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does VVVF stand for?",
+    answer: "Revolution",
+    options: [
+      "AV112-D",
+      "AV003-C",
+      "AV106-C",
+      "AV108-C"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What frequency range is the High Frequency band?",
+    answer: "Tools",
+    options: [
+      "AV003-D",
+      "AV101-A",
+      "AV011-A",
+      "AV004-C"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "The first step to getting output from a laser is to excite an active medium. What is this process called?",
+    answer: "Methods",
+    options: [
+      "AV105-C",
+      "AV111-B",
+      "AV010-A",
+      "AV111-A"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the relationship between resistivity r and conductivity s?",
+    answer: "Macro",
+    options: [
+      "AV009-C",
+      "AV005-A",
+      "AV112-C",
+      "AV014-B"
+    ]
+  },
+];
